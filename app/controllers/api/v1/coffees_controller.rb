895cb829388c1e24de660f07e48f6358ea6f213c
Morrie <morrie@4point-inc.com>
2018-07-26 21:17:23 +0800

    add api

--- app/controllers/api/v1/coffees_controller.rb
+++ app/controllers/api/v1/coffees_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::CoffeesController < ApplicationController
+  def index
+    @coffees = CoffeeDatum.all
+     render json: {
+        data: @coffees
+      }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :coffees
   root "coffees#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :coffees, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
